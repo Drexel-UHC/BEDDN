d8e7fa2f34bad3fa856b1d1d108370fc1665e062
stf45 <stf45@drexel.edu>
2023-10-03 07:38:57 -0700

    requested all years

--- requests/akbar_nejad_yadzi_r/beddn2019_fsu_fsr.py
+++ requests/akbar_nejad_yadzi_r/beddn2019_fsu_fsr.py
@@ -8,10 +8,7 @@ Created on Wed Sep 27 15:49:21 2023
 import pandas as pd
 
 #%% LOAD DATA, SUBSET
-df = pd.read_csv(r'\\files.drexel.edu\colleges\SOPH\Shared\UHC\Projects\NETS\Data\NETS2019_Python\BEDDN_tr10_measures20230914.txt', sep='\t', dtype={'tract10':str})
-
-subset = df.loc[df['Year']==2019]
-subset = subset[['tract10', 'Year', 't10_net_fsr_c', 't10_net_fsu_c']]
+df = pd.read_csv(r'\\files.drexel.edu\colleges\SOPH\Shared\UHC\Projects\NETS\Data\NETS2019_Python\BEDDN_tr10_measures20230914.txt', sep='\t', dtype={'tract10':str}, usecols=['tract10', 'Year', 't10_net_fsr_c', 't10_net_fsu_c'])
 
 #%% EXPORT TO CSV
-subset.to_csv(r'C:\Users\stf45\Documents\BEDDN\requests\akbar_nejad_yadzi_r\BEDDN2019_tr10_fsu_fsr_2019.csv', index=False)
+df.to_csv(r'C:\Users\stf45\Documents\BEDDN\requests\akbar_nejad_yadzi_r\tr10_BEDDN2019_fsu_fsr_2019.csv', index=False)
